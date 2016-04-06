1362dcd0a90499e723d6ad8919cfd0a09fb476bf
Rado Buransky <radoburansky@gmail.com>
2016-04-06 21:12:28 +0200

    Example works

--- examples/src/main/scala/com/pygmalios/reactiveinflux/spark/examples/Example.scala
+++ examples/src/main/scala/com/pygmalios/reactiveinflux/spark/examples/Example.scala
@@ -1,5 +1,45 @@
 package com.pygmalios.reactiveinflux.spark.examples
 
-object Example extends App {
+import com.pygmalios.reactiveinflux._
+import com.pygmalios.reactiveinflux.spark._
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+import org.joda.time.DateTime
 
+import scala.concurrent.duration._
+
+/**
+  * This example requires that:
+  * 1. There exists "application.conf" resource containing Influx URL and other settings.
+  * 2. "Example" DB is created in Influx.
+  */
+object Example extends App {
+  val conf = new SparkConf()
+    .setMaster("local[*]")
+    .setAppName("Example")
+  val sc = new SparkContext(conf)
+
+  val point1 = Point(
+    time        = DateTime.now(),
+    measurement = "measurement1",
+    tags        = Map(
+      "tagKey1" -> "tagValue1",
+      "tagKey2" -> "tagValue2"),
+    fields      = Map(
+      "fieldKey1" -> "fieldValue1",
+      "fieldKey2" -> 10.7)
+  )
+
+  // Provide settings for reactiveinflux
+  implicit val params = ReactiveInfluxDbName("example")
+  implicit val awaitAtMost = 1.second
+
+  // Create RDD with Influx point
+  val rdd: RDD[Point] = sc.parallelize(Seq(point1))
+
+  // Save RDD to Influx
+  rdd.saveToInflux()
+
+  // Stop Spark context
+  sc.stop()
 }
