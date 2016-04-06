bc2efa6a94cb96e2aaef5c529cb504deef70aa19
Rado Buransky <radoburansky@gmail.com>
2016-04-06 21:53:21 +0200

    JavaStreamingExample works

---
+++ examples/src/main/java/com/pygmalios/reactiveinflux/spark/examples/JavaStreamingExample.java
@@ -0,0 +1,60 @@
+package com.pygmalios.reactiveinflux.spark.examples;
+
+import com.pygmalios.reactiveinflux.jawa.JavaPoint;
+import com.pygmalios.reactiveinflux.jawa.Point;
+import com.pygmalios.reactiveinflux.spark.jawa.SparkInflux;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.joda.time.DateTime;
+
+import java.util.*;
+
+public class JavaStreamingExample {
+    public static void main(String[] args) {
+        // Initialize Spark
+        SparkConf conf = new SparkConf()
+                .setMaster("local[*]")
+                .setAppName("Example");
+        JavaStreamingContext ssc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+        // Initialize SparkInflux instance
+        SparkInflux sparkInflux = new SparkInflux("example", 1000);
+
+        // Define tags for the point
+        Map<String, String> tags = new HashMap<>();
+        tags.put("t1", "A");
+        tags.put("t2", "B");
+
+        // Define fields for the point
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("f1", 10.3);
+        fields.put("f2", "x");
+        fields.put("f3", -1);
+        fields.put("f4", true);
+
+        // Write a single point to "measurement1"
+        Point point = new JavaPoint(
+            DateTime.now(),
+            "measurement1",
+            tags,
+            fields
+        );
+
+        // Create DStream of Influx points
+        Queue<JavaRDD<Point>> queue = new LinkedList<>();
+
+        // Add single RDD with a single Influx point to the DStream
+        queue.add(ssc.sparkContext().parallelize(Collections.singletonList(point)));
+        JavaDStream<Point> queueStream = ssc.queueStream(queue);
+
+        // Save RDD to Influx
+        sparkInflux.saveToInflux(queueStream);
+
+        // Start Spark streaming
+        ssc.start();
+        ssc.awaitTermination();
+    }
+}
--- src/main/scala/com/pygmalios/reactiveinflux/spark/jawa/SparkInflux.scala
+++ src/main/scala/com/pygmalios/reactiveinflux/spark/jawa/SparkInflux.scala
@@ -3,8 +3,9 @@ package com.pygmalios.reactiveinflux.spark.jawa
 import com.pygmalios.reactiveinflux.ReactiveInfluxDbName
 import com.pygmalios.reactiveinflux.jawa.{Conversions, PointNoTime}
 import com.pygmalios.reactiveinflux.spark._
-import com.pygmalios.{reactiveinflux => sc}
 import org.apache.spark.api.java.JavaRDD
+import org.apache.spark.streaming.api.java.JavaDStream
+
 import scala.concurrent.duration._
 
 class SparkInflux(val dbName: String,
@@ -14,5 +15,9 @@ class SparkInflux(val dbName: String,
 
   def saveToInflux[T <: PointNoTime](javaRdd: JavaRDD[T]): Unit = {
     javaRdd.rdd.map(Conversions.toScala).saveToInflux()
+  }
+
+  def saveToInflux[T <: PointNoTime](javaDStream: JavaDStream[T]): Unit = {
+    javaDStream.dstream.map(Conversions.toScala).saveToInflux()
   }
 }
